ac5d9a69ebadccd6a99ac5460c3908b5d9d025a9
LB-personal <68199302+LB-personal@users.noreply.github.com>
2026-03-20 15:21:23 +0200

    added a naive single threaded fetcher to the scraper

---
+++ internal/fatcher/fatcher.go
@@ -0,0 +1,77 @@
+package fatcher
+
+import (
+	"context"
+	"errors"
+	"io"
+	"log"
+	"net/http"
+)
+
+type HttpGetter interface {
+	Get(url string) (resp *http.Response, err error)
+}
+
+type fatcher struct {
+	client     HttpGetter
+	urls       <-chan string
+	bufferSize int
+}
+
+type Fatcher interface {
+	Fatch(ctx context.Context) <-chan []byte
+}
+
+func NewFatcher(c HttpGetter, u <-chan string, size int) Fatcher {
+	return &fatcher{
+		client:     c,
+		urls:       u,
+		bufferSize: size,
+	}
+}
+
+func (f *fatcher) Fatch(ctx context.Context) <-chan []byte {
+	c := make(chan []byte, f.bufferSize)
+	go f.internal(ctx, c)
+	return c
+}
+
+func (f *fatcher) internal(ctx context.Context, responses chan<- []byte) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case url, ok := <-f.urls:
+			if !ok {
+				return
+			}
+
+			res, err := f.client.Get(url)
+			if err != nil { // todo: add more rubust error handleing that includes retrys
+				log.Fatal(err)
+			}
+
+			body, err := handleRespone(res) // todo: add more rubust error handleing that includes retrys
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			responses <- body
+		}
+	}
+}
+
+func handleRespone(res *http.Response) ([]byte, error) {
+	defer res.Body.Close()
+
+	if res.StatusCode > 299 {
+		return nil, errors.New("got bad request")
+	}
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	return body, nil
+}
